c281b8fb43728d5816b1348888df160308c60d7f
stanescon <stanescon.n@gmail.com>
2022-03-30 19:00:58 -0300

    adicionando busca

--- entertainment-web-app/src/app/main/main.component.ts
+++ entertainment-web-app/src/app/main/main.component.ts
@@ -9,6 +9,8 @@ import { Component, OnInit } from '@angular/core';
 export class MainComponent implements OnInit {
 
   movieInformation: any[] = []
+  busca = ''
+  movieBusca: any[] = []
 
   constructor(
     private http: HttpClient
@@ -17,6 +19,13 @@ export class MainComponent implements OnInit {
   ngOnInit(): void {
     this.http.get('https://api.themoviedb.org/3/movie/popular?api_key=c486dfa0652725b2074f5636af14ea12&language=pt-BR')
     .subscribe((response: any) => this.movieInformation = response.results)
+  }
+
+  teste(event: any){
+    console.log(event.target.value)
+    this.busca = event.target.value
+    this.http.get(`https://api.themoviedb.org/3/search/movie?api_key=c486dfa0652725b2074f5636af14ea12&language=pt-BR&query=${this.busca}&page=1&include_adult=false`)
+    .subscribe((response: any) => this.movieBusca = response.results)
   }
 
 }
