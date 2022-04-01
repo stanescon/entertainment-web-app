3d0a31e614d7013165c7aff72c98150337e97f3d
stanescon <stanescon.n@gmail.com>
2022-04-01 03:01:28 -0300

    Update server.js

--- entertainment-web-app/server.js
+++ entertainment-web-app/server.js
@@ -1,8 +1,15 @@
+//Install express server
 const express = require('express');
 const path = require('path');
+
 const app = express();
-app.use(express.static(__dirname + '/dist/<app-name>'));
-app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname+
-'/dist/<app-name>/index.html'));});
+
+// Serve only the static files form the dist directory
+app.use(express.static('./dist/entertainment-web-app'));
+
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', {root: 'dist/entertainment-web-app/'}),
+);
+
+// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
